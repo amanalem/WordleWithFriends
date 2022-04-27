d6b7bec6c0efedf07757f7709709245bc0585ac5
aman-alem <alem.aman@gmail.com>
2022-04-27 18:26:24 -0500

    WWF: player can enter a 5 letter word into a text box and submit it with a button click. The word is then stored in a class called word and its letters are pushed into a letters aray inside of the class. When word is submitted, dictionary API lets you know if it is a word or not.

---
+++ app.js
@@ -0,0 +1,34 @@
+let submit = document.querySelector('#submitButton');
+
+submit.addEventListener('click', (e)=> {
+    e.preventDefault();
+    let textInput = document.querySelector('#wordBox').value;
+    // console.log(textInput);
+    if (textInput.length !== 5){
+        alert(`Your word must be exactly 5 letters long. Please try again.`)
+    } else {
+        // Dictionary api url setup
+        let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${textInput}`
+        fetch(url)
+        .then(res => res.json())
+        .then(res => {
+            // console.log(res)
+            if (res.title){
+                alert(`This is not an English word. Please try again`)
+            } else {
+                alert(`Got it! Time to put your friend's skills to the test...`);
+                const word = new Word(textInput);
+                word.letters = textInput.split('');
+                console.log(word.letters);
+            }
+        })
+        .catch(err => console.log(`This is an error!`, err))
+    }  
+});
+
+class Word {
+    constructor(word){
+        this.word = word;
+        this.letters = [];
+    }
+}
